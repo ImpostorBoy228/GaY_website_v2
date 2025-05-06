8ee44894cf591b44e4986ae6a962154a22b143de
ImpostorBoy228 <susman209290@gmail.com>
2025-05-06 21:01:44 +0700

    Delete version after i sucked a cock directory

--- version after i sucked a cock/global_state.py
+++
@@ -1,9 +0,0 @@
-# global_state.py
-import logging
-
-logger = logging.getLogger(__name__)
-
-# Глобальный словарь для хранения состояния задач
-tasks = {}
-
-logger.info("Initialized global_state with empty tasks dictionary")
